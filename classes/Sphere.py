a07ce363216fecc6b14343bdc499def01ab6bea6
idan10000 <49813824+idan10000@users.noreply.github.com>
2022-05-08 14:55:41 +0300

    Changes by Yagon

--- classes/Sphere.py
+++ classes/Sphere.py
@@ -1,5 +1,7 @@
+import numpy as np
+
 class Sphere:
     def __init__(self, cx, cy, cz, radius, material):
-        self.center = (cx, cy, cz)
+        self.center = np.array((cx, cy, cz))
         self.radius = radius
         self.material = material
